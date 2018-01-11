90f882b02d694fec6712c0e7b74674d91c1c6987
gedong <gedong@wanglibank.com>
2018-01-11 14:08:06 +0800

    Initial Commit

---
+++ BTE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTE
+//
+//  Created by wangli on 2018/1/11.
+//  Copyright © 2018年 wangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BTE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTE
+//
+//  Created by wangli on 2018/1/11.
+//  Copyright © 2018年 wangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
