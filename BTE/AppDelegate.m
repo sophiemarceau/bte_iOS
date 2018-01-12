cb3c87de2dcd577a8e51c8b656a6b97da52fe361
gedong <gedong@wanglibank.com>
2018-01-12 10:33:12 +0800

    比特易三方导入完成

--- BTE/AppDelegate.m
+++ BTE/AppDelegate.m
@@ -16,7 +16,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // 启动图片延时: 1秒
+    [NSThread sleepForTimeInterval:3];
+    
+//    [self setupKeyWindow];
+    
+    // 引导图
+//    [self _showGuideView];
+    
+    
     return YES;
 }
 
@@ -46,6 +54,61 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+- (void)setupKeyWindow {
+    
+//    BHTabBarController *tabBarVC = [BHTabBarController new];
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    self.window.rootViewController = tabBarVC;
+    self.window.backgroundColor= [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    //请求版本更细 （注意一定要在初始化网络之后加载）
+//    [BHVersionTool requestAppVersion:tabBarVC];
+}
+
+#pragma mark EAIntroDelegate 版本引导图
+//- (void)_showGuideView {
+//    // 功能简介图
+//    if ([self isFirstLoad]) {
+//        [self showIntroWithCrossDissolve];
+//    }
+//}
+//
+//
+//
+//- (BOOL)isFirstLoad {
+//    NSString *currentVersion = [[[NSBundle mainBundle] infoDictionary]
+//                                objectForKey:@"CFBundleShortVersionString"];
+//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//
+//    NSString *lastRunVersion = [defaults objectForKey:LAST_RUN_VERSION_KEY];
+//    if (!lastRunVersion) {
+//        [defaults setObject:currentVersion forKey:LAST_RUN_VERSION_KEY];
+//        return YES;
+//    } else if (![lastRunVersion isEqualToString:currentVersion]) {
+//        [defaults setObject:currentVersion forKey:LAST_RUN_VERSION_KEY];
+//        return YES;
+//    }
+//    return NO;
+//}
+//
+//-(void)introDidFinish {
+//    //no do somthing
+//}
+//
+//- (void)showIntroWithCrossDissolve {
+//
+//    NSMutableArray *pages = [NSMutableArray array];
+//    for (int i=1; i<=3; i++) {
+//        EAIntroPage *page = [EAIntroPage page];
+//        page.titleImage = [UIImage imageNamed:[NSString stringWithFormat:@"guide%d",i]];
+//        [pages addObject:page];
+//    }
+//
+//    EAIntroView *intro = [[EAIntroView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT) andPages:pages];
+//    intro.delegate=self;
+//    [intro showInView:self.window.rootViewController.view animateDuration:0.0];
+//}
 
 
 @end
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '8.0'
+
+target ‘BTE’ do
+pod 'AFNetworking',  '~> 3.1.0'
+pod 'Masonry',  '~> 1.1.0'
+pod 'SDWebImage',  '~> 4.2.2'
+pod 'MJRefresh',  '~> 3.1.15.1'
+pod 'YYModel',  '~> 1.0.4'
+pod 'MBProgressHUD',  '~> 1.1.0'
+pod 'Bugly',  '~> 2.5.0'
+pod 'WebViewJavascriptBridge',  '~> 6.0.3'
+pod 'SGQRCode',  '~> 2.1.8'
+pod 'UMengUShare/UI'
+pod 'UMengUShare/Social/ReducedWeChat'
+pod 'UMengUShare/Social/ReducedQQ'
+
+end
