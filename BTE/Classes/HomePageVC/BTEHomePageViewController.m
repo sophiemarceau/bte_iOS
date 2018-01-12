e1b74b2acfa9c75b51ea8ca957847da811830b5d
gedong <gedong@wanglibank.com>
2018-01-12 18:52:38 +0800

    返回按钮添加

--- BTE/Classes/HomePageVC/BTEHomePageViewController.m
+++ BTE/Classes/HomePageVC/BTEHomePageViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"比特易";
 }
 
 - (void)didReceiveMemoryWarning {
